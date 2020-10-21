bfab0eb0272c56baca78deab80818f4a0a43fc8d
MadosMark <69796358+MadosMark@users.noreply.github.com>
2020-10-21 14:07:42 +0200

    Update

--- index.php
+++ index.php
@@ -9,25 +9,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Real Fake News</title>
     <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Asap+Condensed&display=swap" rel="stylesheet">
+
 
 
 </head>
 
 <body>
-    <h1> Fake News </h1>
+    <div class="topHeader">
+        <h1> Real Fake News </h1>
+    </div>
+
+    <nav>
+        <ul>
+            <a> Trending </a>
+            <a> World Wide News</a>
+            <a> Real News </a>
+            <a> Music </a>
+            <a> New Tech</a>
+        </ul>
+    </nav>
 
     <div class="grid-container">
-        <div class="grid-item">1</div>
-        <div class="grid-item">2</div>
-        <div class="grid-item">3</div>
-        <div class="grid-item">4</div>
-        <div class="grid-item">5</div>
-        <div class="grid-item">6</div>
-        <div class="grid-item">7</div>
-        <div class="grid-item">8</div>
-        <div class="grid-item">9</div>
+
+        <div class="grid-item">
+            <img class="travis" src="https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/09/0/0/Travis-Scott-MacDonalds-reuters.jpg?ve=1&tl=1" alt="Travis Scott">
+            Travis Scott is opening a brand new Mc Donald's!
+
+
+        </div>
+
+        <div class="grid-item">
+            <img class="KanyeWest" src="https://static.billboard.com/files/media/kanye-west-whh-2018-billboard-1548-compressed.jpg" alt="Kanye West">
+            <Kanye> Kanye West is building a city near you, will you move? </p>
+
+        </div>
+
+
+
     </div>
 </body>
 
