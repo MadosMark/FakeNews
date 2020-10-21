a7976ed27ba24d129104ae04829579fef68346f6
MadosMark <69796358+MadosMark@users.noreply.github.com>
2020-10-21 19:34:44 +0200

    Update

--- index.php
+++ index.php
@@ -25,11 +25,11 @@
 
     <nav>
         <ul>
-            <a> Trending </a>
-            <a> World Wide News</a>
-            <a> Real News </a>
-            <a> Music </a>
-            <a> New Tech</a>
+            <a> Trending &nbsp; &nbsp; <i class="arrow down"></i> </a>
+            <a> World Wide News &nbsp; &nbsp; <i class="arrow down"></i> </a>
+            <a> Real News &nbsp; &nbsp; <i class="arrow down"></i> </a>
+            <a> Music &nbsp; &nbsp; <i class="arrow down"></i> </a>
+            <a> New Tech &nbsp; &nbsp; <i class="arrow down"></i></a>
         </ul>
     </nav>
 
@@ -37,27 +37,65 @@
 
         <div class="grid-item">
             <img class="travis" src="https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2020/09/0/0/Travis-Scott-MacDonalds-reuters.jpg?ve=1&tl=1" alt="Travis Scott">
-            Travis Scott is opening a brand new Mc Donald's!
+            <p> Travis Scott is opening a brand new Mc Donald's! </p>
+
+
 
 
         </div>
 
         <div class="grid-item">
             <img class="KanyeWest" src="https://static.billboard.com/files/media/kanye-west-whh-2018-billboard-1548-compressed.jpg" alt="Kanye West">
-            <Kanye> Kanye West is building a city near you, will you move? </p>
+            <p> Kanye West is building a city near you, will you move? </p>
 
         </div>
 
         <div class="grid-item">
             <img class="drake" src="https://www.rollingstone.com/wp-content/uploads/2019/10/Drake.jpg" alt="Drake">
-            <Kanye> Drake had enough. No more music from Drake! </p>
+            <p> Drake had enough. No more music from Drake! </p>
 
         </div>
 
 
         <div class="grid-item">
             <img class="postMalone" src="https://globalnews.ca/wp-content/uploads/2019/09/saint-tropez-post-malone-2.jpg?quality=85&strip=all" alt="Post Malone">
-            <Kanye> Post Malone is becoming a tattoo artist! </p>
+            <p> Post Malone is becoming a tattoo artist! </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="tyDollaSign" src="https://static.highsnobiety.com/thumbor/s9DM-l2RT_ynEhOKcmX_sDcYoHc=/800x480/static.highsnobiety.com/wp-content/uploads/2018/12/10163219/ty-dolla-sign-indicted-felony-drug-charges-00.jpg" alt="Ty Dolla Sign">
+            <p> Ty Dolla Sign won't eat any more food from now on! </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="bigSean" src="https://www.gannett-cdn.com/presto/2020/09/03/PDTF/bbad2932-c0b6-405b-8242-82f8bac85ed5-031120_big_sean_rg_07.jpg?crop=2399,1350,x0,y122&width=2399&height=1350&format=pjpg&auto=webp" alt="Big Sean">
+            <p> Big Sean thought the world is round, but not anymore! </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="machineGunKelly" src="https://www.rollingstone.com/wp-content/uploads/2020/10/Machine-Gun-Kelly-Press-Photo.jpg" alt="Machine Gun Kelly">
+            <p> Machine Gun Kelly switched from Hip Hop to Rock n Roll! </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="blackBear" src="https://yt3.ggpht.com/a/AATXAJyKj50ZZ9xMEPB2TnzMYosjv2mI4B3GrfvJv-BG=s900-c-k-c0x00ffffff-no-rj" alt="Blackbear">
+            <p> Blackbear had a baby! </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="asapRocky" src="https://static01.nyt.com/images/2019/07/30/arts/30asap-whatweknow-1/30asap-whatweknow-1-superJumbo.jpg" alt="Asap Rocky">
+            <p> Asap Rocky was seen with Kim Kardashian, does Kanye know? </p>
+
+        </div>
+
+        <div class="grid-item">
+            <img class="eminem" src="https://townsquare.media/site/812/files/2020/07/eminem.jpg?w=980&q=75" alt="Eminem">
+            <p> Eminem just became a grandfather! </p>
 
         </div>
 
